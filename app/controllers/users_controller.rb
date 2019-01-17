669e969b1a7673f78c19367dfe533771d05d9a57
Arye Dov Eidelman <aryedov.e@gmail.com>
2019-01-17 18:39:25 -0500

    Create users new page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,33 @@
 class UsersController < ApplicationController
+  def index
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
+
+  def destroy
+
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :calendar_users
   has_many :calendars, through: :calendar_users
+  has_secure_password
 
   validates :name, length: { in: 1..12 }
   validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "invalid email address" }
