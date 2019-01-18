7a23d8220bd4a08cf739c847409744c5dd4d717b
Arye Dov Eidelman <aryedov.e@gmail.com>
2019-01-17 23:14:15 -0500

    Add fixures and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+12.times do |last_month|
+  30.times do |yesterday|
+    HebrewDate.find_or_create_by(month: last_month + 1, day: yesterday + 1, is_adar_b: nil)
+  end
+end
+
+30.times do |yesterday|
+  HebrewDate.find_or_create_by(month: 6, day: yesterday+1, is_adar_b: false)
+end
+
+30.times do |yesterday|
+  HebrewDate.find_or_create_by(month: 6, day: yesterday+1, is_adar_b: true)
+end
