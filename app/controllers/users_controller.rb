34449ead83440ccddf74d75d34ea157ff7165131
Arye Dov Eidelman <aryedov.e@gmail.com>
2019-01-20 16:19:29 -0500

    implement User routes CRUD except delete/destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,33 +1,42 @@
 class UsersController < ApplicationController
   def index
-
+    @users = User.all
   end
 
   def new
     @user = User.new
   end
 
-  def create
-
-  end
-
   def show
-
+    @user = User.find_by(id: params[:id])
   end
 
   def edit
-
-  end
-
-  def update
-
+    @user = User.find_by(id: params[:id])
   end
 
   def delete
+    @user = User.find_by(id: params[:id])
+  end
 
+  def create
+    @user = User.new(user_params)
+    @user.save ? redirect_to(@user) : render(:new)
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.update(user_params) ? redirect_to(@user) : render(:edit)
   end
 
   def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy ? redirect_to(root_path) : render(:delete)
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :email)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,9 @@ class User < ApplicationRecord
   has_many :calendars, through: :calendar_users
   has_secure_password
 
-  validates :name, length: { in: 1..12 }
-  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "invalid email address" }
+  validates :name, :email, :password, presence: true
+
+  validates :name, length: { in: 1..50 }
+  validates :email, format: { with: /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/, message: "is an invalid address" }
+  validates :email, uniqueness: true
 end
